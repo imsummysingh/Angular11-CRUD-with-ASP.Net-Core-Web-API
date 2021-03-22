0725bd80f4d8bb4a8fba456d91d17738d1f1cb14
Summy Singh <summysingh86@gmail.com>
2021-03-22 15:45:14 +0530

    Payment API Added

---
+++ PaymentAPI/Models/PaymentDetailContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetailContext :DbContext
+    {
+        //constructor for DBCOntext
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options):base(options)
+        {
+
+        }
+
+        //property for PaymentDetail
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
